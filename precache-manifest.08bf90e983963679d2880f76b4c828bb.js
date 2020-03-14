6674146853988fb867722945727836ae1f7fbba7
MZO9400 <hamza.hameed00@yahoo.com>
2020-03-14 15:14:46 +0500

    Updates

---
+++ precache-manifest.08bf90e983963679d2880f76b4c828bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2718bd32adf9a844256a59fa449d0771",
+    "url": "/COVID19-Tracker/index.html"
+  },
+  {
+    "revision": "3abf00331e28f031bedb",
+    "url": "/COVID19-Tracker/static/css/main.79dc4e13.chunk.css"
+  },
+  {
+    "revision": "e54d513ddac92aa078c8",
+    "url": "/COVID19-Tracker/static/js/2.b98f8f64.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/COVID19-Tracker/static/js/2.b98f8f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3abf00331e28f031bedb",
+    "url": "/COVID19-Tracker/static/js/main.ca492848.chunk.js"
+  },
+  {
+    "revision": "c7f6e1738b0387ebab05",
+    "url": "/COVID19-Tracker/static/js/runtime-main.7ffeab48.js"
+  }
+]);
